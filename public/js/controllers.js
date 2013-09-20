631e42ebc8e0ee27071ad666cc9d3f1cbc5959cb
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2013-09-20 09:05:51 +0200

    init

---
+++ public/js/controllers.js
@@ -0,0 +1,34 @@
+'use strict';
+
+beerApp.controller('BeersCtrl', function ($scope, Beer) {
+
+	$scope.beers = Beer.query();
+
+});
+
+
+beerApp.controller('EditCtrl', function ($scope, $routeParams, Beer, $location) {
+
+  var beerId = $routeParams.id;
+
+  if (beerId) {
+    Beer.get({id: beerId}, function(beer) {
+      $scope.beer = beer;
+    });
+  } else {
+    $scope.beer = new Beer();
+  }
+
+
+  $scope.save = function(beer) {
+    beer.$save(function() {
+      $location.path('/beer');
+    });
+  };
+
+  $scope.delete = function(beer) {
+    beer.$delete(function() {
+      $location.path('/beer');
+    });
+  };
+});
---
+++ public/js/filters.js
@@ -0,0 +1,16 @@
+'use strict';
+
+beerApp.filter('stars', function () {
+
+    var STARS = {
+        1: '\u2605',
+        2: '\u2605\u2605',
+        3: '\u2605\u2605\u2605',
+        4: '\u2605\u2605\u2605\u2605',
+        5: '\u2605\u2605\u2605\u2605\u2605'
+    };
+
+    return function(startCount) {
+        return STARS[startCount];
+    };
+});
