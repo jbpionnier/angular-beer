7df1e176d7f23cc7982d75b92f5ebe6699c3f0c1
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2013-09-20 09:11:47 +0200

    add routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
 var beerApp = angular.module('beerApp', []);
+
+beerApp.config(function($routeProvider) {
+
+  $routeProvider.
+    when('/beers/', {
+      templateUrl: 'partials/beers.html',
+      controller: 'BeersCtrl'
+    }).
+    when('/beers/:id', {
+      templateUrl: 'partials/edit.html',
+      controller: 'EditCtrl'
+    }).
+    otherwise({
+      redirectTo: '/beers/'
+    });
+
+});
