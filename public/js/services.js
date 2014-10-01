39613ac05dffc9ac2cee847da4bbcc10974308e6
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-10-01 10:40:38 +0200

    Integrating ng-reaccess

--- public/js/services.js
+++ public/js/services.js
@@ -4,4 +4,22 @@ beerApp.factory('Beer', function ($resource) {
 
     return $resource('api/beers/:id', {id: '@id'});
 
+}).factory('Profile', function($http, sfReaccessService) {
+  var _profilePromise = null;
+  return {
+    get: function() {
+      if(_profilePromise) {
+        return _profilePromise;
+      }
+      return _profilePromise = $http.get('/profile').then(function(response) {
+        sfReaccessService.setRights(response.data.rights);
+        sfReaccessService.setValues([{
+          _username: response.data.username
+        }]);
+        return response.data;
+      }, function() {
+        _profilePromise = null;
+      });
+    }
+  }
 });
