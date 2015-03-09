e8f8f4c1ea901b332ace8f3b177b31b1c31e372b
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2015-03-09 22:25:47 +0100

    chore: update angular version

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var beerApp = angular.module('beerApp', []);
+var beerApp = angular.module('beerApp', ['ngRoute']);
 
-beerApp.config(function($routeProvider) {
+beerApp.config(function ($routeProvider) {
 
   $routeProvider.
-    when('/beers/', {
-      templateUrl: 'partials/beers.html',
-      controller: 'BeersCtrl'
-    }).
-    when('/beers/:id', {
-      templateUrl: 'partials/edit.html',
-      controller: 'EditCtrl'
-    }).
-    otherwise({
-      redirectTo: '/beers/'
-    });
+      when('/beers/', {
+        templateUrl: 'partials/beers.html',
+        controller: 'BeersCtrl'
+      }).
+      when('/beers/:id', {
+        templateUrl: 'partials/edit.html',
+        controller: 'EditCtrl'
+      }).
+      otherwise({
+        redirectTo: '/beers/'
+      });
 
 });
